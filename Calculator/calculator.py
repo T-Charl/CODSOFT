1ceba20342c1008c23252d0986e9ddf126fd41b5
Thabani <hlatshie01@gmail.com>
2024-01-11 20:45:59 +0200

    Started on calculator

---
+++ Calculator/calculator.py
@@ -0,0 +1,17 @@
+
+
+
+def main():
+    ...
+
+
+
+def supported_operations():
+    print(" WELCOME TO THE SIMPLE ARITHMETIC CALCULATOR")
+    print("""This calculator supports the operations below:
+ + Addition (returns sum of given integers).
+ - Subtraction (returns the difference between integers).
+ * Multiplication (returns the repeated addition of integers of equal sizes).
+ / Division (returns the sharing of an integer into equal-sized groups).
+ % Modulus (returns the remainder or signed remainder of a division)
+""")
