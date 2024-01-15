2c71cc9ea2b1177ac0d9b7aaaf703590e95bc6d0
Thabani <hlatshie01@gmail.com>
2024-01-16 00:15:59 +0200

    To-DO list

---
+++ To_Do_List/to_do.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+def help():
+    print("""To-DO List Options:
+Add - Add Task
+Add Optionals:
+  -Task Description
+  -Task Due Date
+        
+Edit/Update - Edit Task
+Delete - Delete Task
+Complete - Mark Task as Completed""")
