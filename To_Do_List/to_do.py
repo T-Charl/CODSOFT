482070e5d6fb5ae7f3b9282e7438985a820cfddc
Thabani <hlatshie01@gmail.com>
2024-01-16 22:34:19 +0200

    Added to To_Do list

--- To_Do_List/to_do.py
+++ To_Do_List/to_do.py
@@ -1,4 +1,5 @@
 from datetime import date
+import csv
 
 def help():
     print("""To-DO List Options:
@@ -10,3 +11,46 @@ Add Optionals:
 Edit/Update - Edit Task
 Delete - Delete Task
 Complete - Mark Task as Completed""")
+
+
+def open_csv():
+    
+    with open("To-DO list.csv", "w", newline='') as file:
+        writer = csv.writer(file)
+        headers = 'Time','Task', 'Description', 'Progress'
+        writer.writerow(headers)
+
+
+def add_task():
+    with open("To-DO list.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(task_info())
+
+            
+# def update_task():
+    
+
+def display_task():
+    with open("To-DO list.csv", "r", newline="") as file:
+        reader = csv.reader(file)
+        for _ in list(reader)[1:]:
+            print(_)
+
+
+def task_info():
+    task = input("What task would you like to add? ").strip()
+    descrip = input("Task description... ").strip()
+    time = input("Task time... ").strip()
+
+    return time ,task, descrip 
+
+
+def main():
+    add_task()
+    display_task()
+
+
+display_task()
+
+# if __name__ == "__main__":
+#     main()
