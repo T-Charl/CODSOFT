9e3da606797e283230ba9a7d4badc63f64a7a87c
T-Charl <146937383+T-Charl@users.noreply.github.com>
2024-01-30 15:17:44 +0000

    Deleting function added

--- Contact_book/contact_book.py
+++ Contact_book/contact_book.py
@@ -1,5 +1,23 @@
 import csv
+import sys
 import pandas
+
+
+
+def main():
+    user =input("""Welcome to Contact Book.
+Would you like to create your personal contact book? Y/n? """)
+    
+    while True:
+        if user.lower() == 'n':
+            sys.exit("Thank You.\nPlease try again when you would like to make a contact book.")
+        elif user.lower() == 'y':
+            print("Let's get started")
+            add_contact()
+        else:
+            user = input('Would you like to create your personal contact book? Y/n?')
+
+
 
 
 def create_contact_book():
@@ -24,15 +42,31 @@ def add_contact():
 # add_contact()
         
 
-def delete_contact():
-    contact_name = input().strip().title()
+def delete_contact(contact_name):
+    # contact_name = input().strip().title()
 
 
-    delete = pandas.read_csv('Contact_book\contact_book.csv',  index_col='name')
+    delete = pandas.read_csv('/workspaces/CODSOFT/Contact_book/contact_book.csv',  index_col='name')
     delete = delete.drop(contact_name)
     # delete = delete.drop(columns=['Unnamed: 0'])
 
 # Save the DataFrame to a CSV file without including the index
-    delete.to_csv('Contact_book\contact_book.csv', index=True)
+    delete.to_csv('/workspaces/CODSOFT/Contact_book/contact_book.csv', index=True)
     # delete.to_csv('Contact_book\contact_book.csv')
-delete_contact()
+# delete_contact()
+
+
+def read_csv():
+    with open('/workspaces/CODSOFT/Contact_book/contact_book.csv', 'r') as book:
+        red = csv.reader(book)
+        for i in red:
+            if 'False' in i:
+                delete_contact(i[0])
+            else:
+                print(i)
+
+# read_csv()
+
+
+if __name__  == "__main__":
+    main()
